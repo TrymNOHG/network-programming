b6a75fa7576115ffb8c8437897725141ffc498cc
TrymNOHG <trym.gudvangen@gmail.com>
2023-03-09 12:59:46 +0100

    fix: build and run docker synchronously

--- exercise-5/server.js
+++ exercise-5/server.js
@@ -2,30 +2,32 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
-const { spawn } = require('child_process');
+const { execSync, spawn } = require('child_process');
 const fs = require('fs').promises;
 
 app.use(express.static(path.join(__dirname, 'client/build')))
 app.use(bodyParser.json())
 
 // The endpoint of the API
-app.post('/compile', (req, res) => {
+app.post('/compile', async (req, res) => {
     const userCode = req.body.code;
 
     try{
-        fs.writeFile('main.cpp', userCode)
-            .then(r => console.log('Main.cpp created!'));
+        await fs.writeFile('main.cpp', userCode)
+            .then(r => console.log('Main.cpp successfully created!'));
     } catch (error) {
         console.log('Error', error)
     }
     //docker build -t comp . && docker run --rm comp
+    execSync('docker build . -t comp');
+
     const process = spawn('docker', [
         'run',
         '--rm',
         'comp'
     ]);
 
-    process.stdout.on('data', data => {
+    process.stdout.on('data', async data => {
         console.log(`stdout: ${data}`);
         res.send(data);
     });
@@ -37,7 +39,7 @@ app.post('/compile', (req, res) => {
     process.on('close', code => {
         try{
             fs.unlink('./main.cpp')
-                .then(r => console.log('Compiled File Removed.'));
+                .then(r => console.log('Main.cpp has been removed'))
         } catch (error) {
             console.log('Error', error)
         }
@@ -46,8 +48,6 @@ app.post('/compile', (req, res) => {
 
 
 });
-
-
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
